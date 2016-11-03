73fbbab07b4657e174970f48e9deac387c7a3f60
vaidotasp <vaidotasp@gmail.com>
2016-11-03 18:59:07 -0400

    Solution using .reduce()

---
+++ sum-numbers-in-a-range.js
@@ -0,0 +1,19 @@
+function sumAll(arr) {
+  
+  var large = (arr[0] === (Math.max(...arr))) ? arr[0] : arr[1];
+  var small = (arr[0] !== (Math.max(...arr))) ? arr[0] : arr[1];
+	var newArr = []
+	while (large >= small){
+		newArr.push(small);
+		small = small + 1;
+	}
+	
+	var sum = newArr.reduce(add, 0) 
+	function add(a,b){
+		return a+b;
+	}
+	
+  return sum;
+}
+
+sumAll([1, 33]);
