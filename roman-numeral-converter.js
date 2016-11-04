2eae77b6369ba8b4ef1f559c0cb949824b87d3b4
Vaidotas Piekus <vaidotasp@gmail.com>
2016-11-04 23:07:05 +0000

    initial setup

---
+++ roman-numeral-converter.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Convert the given number into a roman numeral.
+All roman numerals answers should be provided in upper-case.
+*/
+
+function convertToRoman(num) {
+ let dictionary = {
+     1: 'I',
+     2: 'II',
+     3: 'III',
+     4: 'IV',
+     5: 'X',
+ };
+ 
+ console.log(dictionary[5]);
+}
+
+
+convertToRoman(36);
