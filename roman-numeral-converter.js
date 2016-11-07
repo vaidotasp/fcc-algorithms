b1e6ed52cc6e5219b169561193a5a5646b0c89a0
Vaidotas Piekus <vaidotasp@gmail.com>
2016-11-07 18:41:58 +0000

    switch implementation logic

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -3,8 +3,6 @@
 /* Convert the given number into a roman numeral.
 All roman numerals answers should be provided in upper-case.
 */
-
-function convertToRoman(num) {
  let dictionary = {
      1: 'I',
      2: 'II',
@@ -16,20 +14,114 @@ function convertToRoman(num) {
      8: 'VIII',
      9: 'IX',
      10: 'X',
-     11: 'XI',
-     12: 'XII',
-     13: 'XIII',
-     14: 'XIV',
-     15: 'XV',
-     16: 'XVI',
-     17: 'XVII',
-     18: 'XVIII',
-     19: 'XIX',
-     20: 'XX'
- };
+     20: 'XX',
+     30: 'XXX',
+     40: 'XL',
+     50: 'L',
+     60: 'LX',
+     70: 'LXX',
+     80: 'LXXX',
+     90: 'XC',
+     100: 'C',
+     200: 'CC',
+     300: 'CCC',
+     400: 'CD',
+     500: 'D',
+     600: 'DC',
+     700: 'DCC',
+     800: 'DCCC',
+     900: 'CM',
+     1000: 'M',
+     2000: 'MM',
+     3000: 'MMM',
+     4000: 'MMMM',
+  };
+  
+  
+function convertToRoman(num) {
+ let result;
+
  
- console.log(dictionary[5]);
+ let numberToString = num.toString();
+  
+  switch (numberToString.length) {
+    case 1:
+      result = dictionary[numberToString];
+      break;
+    case 2:
+      result = dictionary[numberToString[0] +'0'];
+      if (numberToString[1] === '0') {
+        console.log('zero found!');
+        break;
+      } else {
+        result = result + dictionary[numberToString[1]];
+      }
+      break;
+    case 3:
+      result = dictionary[numberToString[0] +'0'+'0'];
+      if (numberToString[1] === '0' && numberToString[2] === '0') {
+        break;
+      } else if (numberToString[1] !== '0' && numberToString[2] === '0') {
+        result = result + dictionary[numberToString[1] + '0'];
+        break;
+      } else if (numberToString[1] === '0' && numberToString[2] !== '0') {
+        result = result + dictionary[numberToString[2]];
+        break;
+      } else {
+        result = result + dictionary[numberToString[1] + '0'] + dictionary[numberToString[2]];
+      }
+    case 4:
+      //X000
+      result = dictionary[numberToString[0] + '0' + '0' + '0'];
+      if (numberToString[1] === '0' && numberToString[2] === '0' &&
+      numberToString[3] === '0') {
+        break;
+      //X00X
+      } else if (numberToString[0] !== '0' && numberToString[1] === '0' &&
+      numberToString[2] === '0' && numberToString[3] !== '0') {
+        result = result + dictionary[numberToString[3]];
+        break;
+      //XX00  
+      } else if (numberToString[0] !== '0' && numberToString[1] !== '0' &&
+      numberToString[2] === '0' && numberToString[3] === '0') {
+        result = result + dictionary[numberToString[1] + '0' +'0'];
+        break;
+      
+      //XXX0  
+      } else if (numberToString[0] !== '0' && numberToString[1] !== '0' &&
+      numberToString[2] !== '0' && numberToString[3] === '0') {
+        result = result + dictionary[numberToString[1] +'0'+'0'] + 
+        dictionary[numberToString[2] +'0'];
+        break;
+      //XX0X
+      } else if (numberToString[0] !== '0' && numberToString[1] !== '0' &&
+      numberToString[2] === '0' && numberToString[3] !== '0') {
+        result = result + dictionary[numberToString[1] + '0' +'0'] + 
+        dictionary[numberToString[3]];
+        break;
+      //X0X0
+      } else if (numberToString[0] !== '0' && numberToString [1] === '0' &&
+      numberToString[2] !== '0' && numberToString[3] === '0') {
+        result = result + dictionary[numberToString[2] + '0'];
+        break;
+      //X0XX  
+      } else if (numberToString[0] !== '0' && numberToString[1] === '0' &&
+      numberToString[2] !== '0' && numberToString[3] !== '0') { 
+        result = result + dictionary[numberToString[2] +'0'] + 
+        dictionary[numberToString[3]];
+        break;
+      // XXXX
+      } else {
+        result = result + dictionary[numberToString[1] + '0' +'0'] + 
+        dictionary[numberToString[2] + '0'] + dictionary[numberToString[3]];
+        
+      }
+      break;
+    default:
+      console.log('unsupported input');
+  }  
+
+return console.log(result);
 }
 
-
-convertToRoman(36);
+convertToRoman(1023);
