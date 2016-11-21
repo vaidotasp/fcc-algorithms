e57197946fb2b582cf559e7ce307238f781f5d30
Vaidotas Piekus <vaidotasp@gmail.com>
2016-11-21 20:37:33 +0000

    working implementation with .filter()

--- art-thou.js
+++ art-thou.js
@@ -2,32 +2,22 @@
 function whatIsInAName(collection, source) {
   var arr = [];
   let target = Object.keys(source);
+  console.log(target.length);
   
-  console.log(target[1]);
-  // console.log(target);
-  for (let prop in collection) {
-    for (let key in collection[prop]) {
-      for (let i=0; i<target.length;i++) {
-        if (key === target[i]) {
-          console.log('key match found', key)
-          console.log(source[target]);
-            if (source[target[i]] === collection[prop][key]) {
-              console.log('perfect match', collection[prop][key]);
-              console.log('returning the whole object', collection[prop]);
-              arr.push(collection[prop]);
-        }
+  
+  return collection.filter(function (obj) {
+    for (let i=0;i<target.length;i++) {
+      if (!obj.hasOwnProperty(target[i]) || obj[target[i]] !== source[target[i]]) {
+        return false
       }
     }
-    }
-    // console.log(collection[prop]);
-    
-
-  }
-  
-  // Only change code above this line
-  return arr;
+    return true
+  });
 }
+  
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, 
-{ first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], 
-{ first: "Mercutio", last: "Capulet"}));
+
+console.log(whatIsInAName([{ "a": 1, "b": 2 }, 
+{ "a": 1 }, 
+{ "a": 1, "b": 2, "c": 2 }], 
+{ "a": 1, "b": 2 }));
