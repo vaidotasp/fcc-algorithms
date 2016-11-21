47f386bc7ea235d306dd2bb71ea7f46ccc990235
Vaidotas Piekus <vaidotasp@gmail.com>
2016-11-21 15:32:04 +0000

    Implementation for N objects in source property

--- art-thou.js
+++ art-thou.js
@@ -2,19 +2,22 @@
 function whatIsInAName(collection, source) {
   var arr = [];
   let target = Object.keys(source);
-  console.log(target[0]);
+  
+  console.log(target[1]);
   // console.log(target);
   for (let prop in collection) {
     for (let key in collection[prop]) {
-      if (key === target[0]) {
-        console.log('key match found', key)
-        console.log(source[target]);
-        if (source[target] === collection[prop][key]) {
-          console.log('perfect match', collection[prop][key]);
-          console.log('returning the whole object', collection[prop]);
-          arr.push(collection[prop]);
+      for (let i=0; i<target.length;i++) {
+        if (key === target[i]) {
+          console.log('key match found', key)
+          console.log(source[target]);
+            if (source[target[i]] === collection[prop][key]) {
+              console.log('perfect match', collection[prop][key]);
+              console.log('returning the whole object', collection[prop]);
+              arr.push(collection[prop]);
         }
       }
+    }
     }
     // console.log(collection[prop]);
     
@@ -27,4 +30,4 @@ function whatIsInAName(collection, source) {
 
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, 
 { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], 
-{ last: "Capulet"}));
+{ first: "Mercutio", last: "Capulet"}));
