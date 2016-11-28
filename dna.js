42ad81d6d78011ff0c5d64b3400f1541d8554448
Vaidotas Piekus <vaidotasp@gmail.com>
2016-11-28 14:34:09 +0000

    implementation with second for loop

--- dna.js
+++ dna.js
@@ -1,21 +1,19 @@
 // DNA Pairings using 2d array
 'use strict'
 function pairElement(str) {
-  
+
   let answer =[];
   let newStr = str.split('');
-  console.log(newStr)
+  
   for (let i=0; i<newStr.length; i++) {
       switch (newStr[i]) {
           case "A":
             answer.push(newStr[i]);
             answer.push("T")
-            answer.splice(0,2, "A", "T");
-            // code
             break;
           case "T":
             answer.push(newStr[i]);
-            answer.push("A")
+            answer.push("A");
             break;
           case "C":
             answer.push(newStr[i]);
@@ -27,15 +25,18 @@ function pairElement(str) {
             break;
       }
   }
+
   let choppedStr = []
-  choppedStr.push(answer.slice(0,2));
-  choppedStr.push(answer.slice(2,4));
-  choppedStr.push(answer.slice(4,6));
-  choppedStr.push(answer.slice(6,8));
-  choppedStr.push(answer.slice(8,10));
-  console.log(answer)
-  console.log(choppedStr);
+  for (let i=0; i<=answer.length;i++) {
+    choppedStr.push(answer.splice(0,2));
+    
+    }
+  choppedStr.push(answer);
+
+
   return choppedStr;
 }
 
 pairElement("ATCGA");
+
+
