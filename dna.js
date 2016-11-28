1a171ccab17189b4a1dcdc8f9e22a82c552ce2a4
Vaidotas Piekus <vaidotasp@gmail.com>
2016-11-28 14:08:16 +0000

    working version, incremental pushing to array

--- dna.js
+++ dna.js
@@ -1,1 +1,40 @@
 // DNA Pairings using 2d array
+'use strict'
+function pairElement(str) {
+  
+  let answer =[];
+  let newStr = str.split('');
+  console.log(newStr)
+  for (let i=0; i<newStr.length; i++) {
+      switch (newStr[i]) {
+          case "A":
+            answer.push(newStr[i]);
+            answer.push("T")
+            // code
+            break;
+          case "T":
+            answer.push(newStr[i]);
+            answer.push("A")
+            break;
+          case "C":
+            answer.push(newStr[i]);
+            answer.push("G")
+            break;
+          case "G":
+            answer.push(newStr[i]);
+            answer.push("C")
+            break;
+      }
+  }
+  let choppedStr = []
+  choppedStr.push(answer.slice(0,2));
+  choppedStr.push(answer.slice(2,4));
+  choppedStr.push(answer.slice(4,6));
+  choppedStr.push(answer.slice(6,8));
+  choppedStr.push(answer.slice(8,10));
+  console.log(answer)
+  console.log(choppedStr);
+  return choppedStr;
+}
+
+pairElement("ATCGA");
